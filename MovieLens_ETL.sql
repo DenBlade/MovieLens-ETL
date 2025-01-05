68bc704927bf82349cb368b4b2cb453190a75c58
DenBlade <denys.klinkov@gmail.com>
2025-01-05 22:13:47 +0200

    Staging area

--- MovieLens_ETL.sql
+++ MovieLens_ETL.sql
@@ -6,8 +6,8 @@ CREATE OR REPLACE TABLE users_staging(
     user_id INTEGER NOT NULL PRIMARY KEY,
     age_id INTEGER NOT NULL FOREIGN KEY REFERENCES age_group_staging(age_group_id),
     gender VARCHAR(1) NOT NULL,
-    zip_code VARCHAR(255) NOT NULL,
-    occupation_id INTEGER NOT NULL FOREIGN KEY REFERENCES occupations_staging(occupation_id)
+    occupation_id INTEGER NOT NULL FOREIGN KEY REFERENCES occupations_staging(occupation_id),
+    zip_code VARCHAR(255) NOT NULL
 );
 CREATE OR REPLACE TABLE age_group_staging(
     age_group_id INTEGER NOT NULL PRIMARY KEY,
@@ -46,5 +46,20 @@ CREATE OR REPLACE TABLE genres_movies_staging(
     genre_id INTEGER NOT NULL FOREIGN KEY REFERENCES genres_staging(genre_id)
 );
 
-CREATE OR REPLACE STAGE temporary_stage;
-
+CREATE STAGE IF NOT EXISTS temporary_stage;
+LIST @temporary_stage;
+CREATE OR REPLACE FILE FORMAT CSV_MovieLens_Format
+    TYPE = CSV
+    COMPRESSION = NONE
+    SKIP_HEADER = 1
+    FIELD_DELIMITER = '|'
+    FIELD_OPTIONALLY_ENCLOSED_BY = '"';
+    
+COPY INTO users_staging FROM @temporary_stage/users.csv FILE_FORMAT = (FORMAT_NAME = CSV_MovieLens_Format) ON_ERROR='CONTINUE';
+COPY INTO genres_staging FROM @temporary_stage/genres.csv FILE_FORMAT = (FORMAT_NAME = CSV_MovieLens_Format) ON_ERROR='CONTINUE';
+COPY INTO movies_staging FROM @temporary_stage/movies.csv FILE_FORMAT = (FORMAT_NAME = CSV_MovieLens_Format) ON_ERROR='CONTINUE';
+COPY INTO genres_movies_staging FROM @temporary_stage/genres_movies.csv FILE_FORMAT = (FORMAT_NAME = CSV_MovieLens_Format) ON_ERROR='CONTINUE';
+COPY INTO ratings_staging FROM @temporary_stage/ratings.csv FILE_FORMAT = (FORMAT_NAME = CSV_MovieLens_Format) ON_ERROR='CONTINUE';
+COPY INTO tags_staging FROM @temporary_stage/tags.csv FILE_FORMAT = (FORMAT_NAME = CSV_MovieLens_Format) ON_ERROR='CONTINUE';
+COPY INTO age_group_staging FROM @temporary_stage/age_group.csv FILE_FORMAT = (FORMAT_NAME = CSV_MovieLens_Format) ON_ERROR='CONTINUE';
+COPY INTO occupations_staging FROM @temporary_stage/occupations.csv FILE_FORMAT = (FORMAT_NAME = CSV_MovieLens_Format) ON_ERROR='CONTINUE';
